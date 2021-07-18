c0bf9f3d8caa025c75468fb699672cad580a66b0
bond1999 <46317327+bond1999@users.noreply.github.com>
2021-07-18 16:46:30 -0500

    Merging new changes

---
+++ Eclipse Project/src/main/workers.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
+public class workers {
+	class Clock extends JPanel {
+		public Clock() {
+			
+		}
+	}
+	
+	class Stats {
+		public Stats() {
+			
+		}
+	}
+}
